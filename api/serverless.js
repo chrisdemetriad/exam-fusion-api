b3de430894cb199d4e46399ff95a64ea31ed2466
Chris <chris@demetriad.co.uk>
2024-11-23 19:44:59 +0000

    refactoring serverless related files

--- api/serverless.js
+++ api/serverless.js
@@ -2,12 +2,13 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import Fastify from "fastify";
+import testRoutes from "../src/routes/test.routes";
 
 const app = Fastify({
 	logger: true,
 });
 
-app.register(import("./index.js"));
+app.register(testRoutes);
 
 export default async (req, res) => {
 	await app.ready();
