2071f0087b1157047d9800f86b4ddaa416dddfd2
Chris <chris@demetriad.co.uk>
2024-11-24 17:46:20 +0000

    renaming env variable

--- src/data/seed.js
+++ src/data/seed.js
@@ -17,7 +17,7 @@ const loadTestData = (fileName) => {
 
 const seedDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_URI);
+		await mongoose.connect(process.env.MONGODB_URI);
 		await Test.deleteMany({});
 		await TestAttempt.deleteMany({});
 
--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const connectToMongo = async () => {
 	if (isMongoConnected) return;
 
 	try {
-		await mongoose.connect(process.env.MONGO_URI, {
+		await mongoose.connect(process.env.MONGODB_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});
