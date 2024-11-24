317cb05cce9aa3d8d3fd7ed71d861ddd02e1dcc4
Chris <chris@demetriad.co.uk>
2024-11-24 17:28:46 +0000

    quickly adding db connection check to verify on Vercel

--- src/routes/test.routes.js
+++ src/routes/test.routes.js
@@ -1,8 +1,32 @@
 const testController = require("../controllers/test.controller");
-
+const mongoose = require("mongoose");
 const PREFIX = "/api/v1/tests";
 
 async function testRoutes(fastify, options) {
+	fastify.get("/status", async (request, reply) => {
+		try {
+			const mongoStatus = mongoose.connection.readyState;
+
+			if (mongoStatus !== 1) {
+				return reply.status(500).send({
+					status: "Error",
+					message: "Database isn't really connected - duh",
+				});
+			}
+
+			return reply.send({
+				status: "API is running",
+				mongoStatus: "Connected",
+			});
+		} catch (error) {
+			return reply.status(500).send({
+				status: "Error",
+				message: "Test check failed",
+				error: error.message,
+			});
+		}
+	});
+
 	fastify.get(`${PREFIX}/all`, testController.getAllProviders);
 	fastify.get(`${PREFIX}/:provider`, testController.getTestsByProvider);
 	fastify.get(`${PREFIX}/:provider/:id`, testController.getTestById);
